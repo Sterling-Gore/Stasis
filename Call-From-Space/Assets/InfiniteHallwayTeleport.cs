fa9ddebe4873e0edfb77b656a9e5acb855027b56
Kevin Duong <kevinmisa1@yahoo.com>
2025-04-26 21:43:35 -0500

    Infinite hallway

---
+++ Call-From-Space/Assets/InfiniteHallwayTeleport.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteHallwayTeleport : MonoBehaviour
+{
+    [SerializeField]
+    Transform teleportPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+       
+        Vector3 relativePosition = transform.InverseTransformPoint(other.transform.position);
+
+        other.transform.parent.GetComponent<Rigidbody>().position = teleportPoint.TransformPoint(relativePosition);
+    }
+}
